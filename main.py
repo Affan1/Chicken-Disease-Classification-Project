f7526bc147fd7bbe3240f43350ce3463d9574ea5
Muhammad Affan Raza <muhammadaffanraza@gmail.com>
2023-11-20 18:56:59 +0000

    Prepare base model

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from ChickenDiseaseClassification import logger
+from ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDiseaseClassification.pipeline.stage_02_base_model_preparation import PrepareBaseModelTrainingPipeline
 
-logger.info("Welcome to my Custom Log")
+STAGE_NAME = "Data Ingestion stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
