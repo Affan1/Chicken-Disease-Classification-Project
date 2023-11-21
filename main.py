9049ae6b3ae0a5587aa1c42ad467c2618aab518a
Muhammad Affan Raza <muhammadaffanraza@gmail.com>
2023-11-21 22:41:12 +0000

    evaluation complete

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ChickenDiseaseClassification import logger
 from ChickenDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassification.pipeline.stage_02_base_model_preparation import PrepareBaseModelTrainingPipeline
 from ChickenDiseaseClassification.pipeline.stage_03_training import ModelTrainingPipeline
+from ChickenDiseaseClassification.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 if __name__ == '__main__':
@@ -32,6 +33,18 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
